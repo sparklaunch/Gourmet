bb2fb911a2e290a475b5a635b17c6dd556adcf4c
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-08-27 09:33:53 +0900

    Create Category and Restaurant models

--- Michelin/models.py
+++ Michelin/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 100)
+    link = models.CharField(max_length = 500)
+    description = models.TextField()
+    keyword = models.CharField(max_length = 50)
+    category = models.ForeignKey(Category, on_delete = models.SET_DEFAULT, default = 3)
